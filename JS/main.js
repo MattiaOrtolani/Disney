15e1064a06c8dbcdd9bf3f606d9eccef7b45e3aa
5kul1y <mattia.ortolani05@gmail.com>
2025-03-31 23:05:56 +0200

    add: first carousel

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,9 @@
 // import { createFilmCards } from './carusel/carousel.js';
 import { api } from './api/api.js';
+import { apiHeroCarousel } from './api/apiHeroCarousel.js';
 
 api();
+apiHeroCarousel();
 
 // Avvia la funzione al caricamento del DOM
 // document.addEventListener('DOMContentLoaded', createFilmCards);
