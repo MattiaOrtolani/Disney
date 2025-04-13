d57e6e5d777c2233a0cd82f5ea59da84b8c5692b
5kul1y <mattia.ortolani05@gmail.com>
2025-04-13 15:22:13 +0200

    add mobile page

--- JS/module/home.js
+++ JS/module/home.js
@@ -2,7 +2,9 @@ import { filmSection } from '../pages/home/film/newsCarousel.js';
 import { filmHero } from '../pages/home/hero/heroCarousel.js';
 import { leftBtnSlide } from '../pages/home/hero/buttonSlide.js';
 import { rightBtnSlide } from '../pages/home/hero/buttonSlide.js';
+import { redirectIfMobile } from '../pages/mobile/mobile.js';
 
+redirectIfMobile();
 filmHero();
 filmSection();
 leftBtnSlide();
---
+++ JS/pages/mobile/mobile.js
@@ -0,0 +1,9 @@
+export function redirectIfMobile()
+{
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile)
+    {
+        window.location.href = '../../../pages/mobile.html';
+    }
+}
